aab806c159bb1355e32b74ffe71202a1b02cc268
Yiwen Zhu <yiwzhu@seas.upenn.edu>
2021-01-15 17:46:25 -0500

    Add post('/search-school') & Modify school package parameters for '/your-schools'

--- filter_results.js
+++ filter_results.js
@@ -1,18 +1,18 @@
 
-//Load packages
+/* Load packages */
 var mysql = require('mysql'); 
 var express = require('express');
 var http = require('http');
 var parser = require('body-parser');
 
-//set up express framework
+/* set up express framework */
 var app = express();
 app.set('view engine', 'ejs');
 app.use(parser.urlencoded({extended: true}));
 //app.use(express.static(__dirname + '/style'))
 app.use(express.static(__dirname))
 
-//make connection with AWS RDS
+/* make connection with AWS RDS */
 var connection = mysql.createConnection({
 	host: 'us-uni.cgcckfsxakvi.us-east-1.rds.amazonaws.com', 
 	user: 'admin', 
@@ -21,7 +21,7 @@ var connection = mysql.createConnection({
 	multipleStatements: true
 });
 
-//function to obtain probabilities given a z-score
+/* function to obtain probabilities given a z-score */
 function getProb(z){
 	if (z < -6.5) return 0.0;
 	if (z > 6.5) return 1.0;
@@ -45,12 +45,12 @@ function getProb(z){
 
 //console.log(getProb(-0.675)); //check if getProb() works
 
-//go to home page index.html
+/* go to home page index.html */
 app.get('/index', function(req, res){
 	res.render('index.html');
 });
 
-//after user inputs their information
+/* recommend schools based on user input scores */
 app.post("/your-schools", function(req, res){
 	var scores = {
 		test: req.body.test_type, 
@@ -116,17 +116,60 @@ app.post("/your-schools", function(req, res){
 		console.log("safety schools: \n")
 		console.log(safety.slice(0, 10));
 
+		//prepare parameters to be sent to your-schools.ejs
+		var school_package = {
+			reach: reach.slice(0, 10),
+			match: match.slice(0, 10),
+			safety: safety.slice(0, 10)
+		}
 		//res.send("TODO: pass along school results to page2.ejs");
-		res.render("your-schools", {reach_schools: reach});
+		res.render('your-schools', {schools: school_package});
 	});
 });
 
-/*
+/* function to translate admission condition */
+function getAdmCon(value){
+	if (value === 1) return 'Required';
+	else if (value === 2) return 'Recommended';
+	else if (value == 5) return 'Not required but considered';
+	else return 'Not required';
+}
+
+/* function to display essay requirements */
+function getEssayReq(number){
+	if (number < 9999) return number;
+	return 'Please check the school website';
+}
+
 //after user searches for a specific school (search bar)
 app.post('/search-school', function(req, res){
-	var instid = 
+	var name = req.body.school_name;
+	var q = `SELECT * FROM uni WHERE INSTNM like '%${name}%';`;
+	connection.query(q, function(error, results){
+		if (error) throw error;
+		//console.log(results);
+		var school_params = {
+			name: results[0].INSTNM, 
+			logo: results[0].IMAGE + ".png", 
+			addr: results[0].ADDR + ', ' + results[0].CITY + ', ' + results[0].STABBR, 
+			rank: results[0].RANKING, 
+			tuition_in: results[0].TUITION2, 
+			tuition_out: results[0].TUITION3, 
+			admr: (results[0].ADMR * 100).toFixed(2),
+			hsgpa: getAdmCon(results[0].ADMCON1),
+			hsrank: getAdmCon(results[0].ADMCON2),
+			hsrecord: getAdmCon(results[0].ADMCON3),
+			prep: getAdmCon(results[0].ADMCON4),
+			recom: getAdmCon(results[0].ADMCON5),
+			test: getAdmCon(results[0].ADMCON7),
+			toefl: getAdmCon(results[0].ADMCON8), 
+			long_essay: getEssayReq(results[0].ESSAYL), 
+			short_essay: getEssayReq(results[0].ESSAYS)
+		};
+		res.render('search-school', {school: school_params});
+	});
 });
-*/
+
 http.createServer(app).listen(80);
 
 
